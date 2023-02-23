ea0455b55ab8cabb16f331d616cbcde4c52ba909
Slavisa Petrovic <slavisa.petrovic@elephantsolutions.net>
2023-02-23 15:43:49 +0100

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+
+const authenticateUser = async (req, res, next) => {
+  // check header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // attach the user to the job routes
+    const testUser = payload.userId === '63f76a9d4e6d8925b8c33648';
+    req.user = { userId: payload.userId, testUser };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+};
+
+module.exports = authenticateUser;
